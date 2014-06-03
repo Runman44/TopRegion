a8da94ecaa0260668406d0c69327da6f28afc46c
Dennis Anderson <Runman@Denniss-Mac.local>
2014-06-03 09:59:28 -0700

    [REQ 1] Added PhotographCDTVC

---
+++ PhotographCDTVC.h
@@ -0,0 +1,15 @@
+//
+//  PhotographCDTVC.h
+//  TopRegion
+//
+//  Created by Dennis Anderson on 6/3/14.
+//  Copyright (c) 2014 MrAnderson. All rights reserved.
+//
+
+#import "CoreDataTableViewController.h"
+
+@interface PhotographCDTVC : CoreDataTableViewController
+
+@property (nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
---
+++ PhotographCDTVC.m
@@ -0,0 +1,40 @@
+//
+//  PhotographCDTVC.m
+//  TopRegion
+//
+//  Created by Dennis Anderson on 6/3/14.
+//  Copyright (c) 2014 MrAnderson. All rights reserved.
+//
+
+#import "PhotographCDTVC.h"
+#import "Photograph.h"
+
+@interface PhotographCDTVC ()
+
+@end
+
+@implementation PhotographCDTVC
+
+-(void) setManagedObjectContext:(NSManagedObjectContext *)managedObjectContext
+{
+    _managedObjectContext = managedObjectContext;
+    
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Photograph"];
+    request.predicate = nil;
+    request.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES selector:@selector(localizedStandardCompare:)]];
+    
+    self.fetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:request managedObjectContext:managedObjectContext sectionNameKeyPath:nil cacheName:nil];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"Photograph Cell"];
+    
+    Photograph *photograph = [self.fetchedResultsController objectAtIndexPath:indexPath];
+    cell.textLabel.text = photograph.name;
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%d photos",[photograph.photos count]];
+    
+    return cell;
+}
+
+@end
